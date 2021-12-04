e7c0fad2b7a97a735e7119a1ab8ec1e7ea89bd8e
Cyril <cyril.novel@kosmon.fr>
2021-12-04 07:58:43 +0100

    Better code

--- 01/01.py
+++ 01/01.py
@@ -1,19 +1,19 @@
 import time
 
 
-def part_1(lines):
+def part_1(values):
     count_increase = 0
-    for i in range(1, len(lines)):
-        if int(lines[i]) > int(lines[i - 1]):
+    for i in range(1, len(values)):
+        if values[i] > values[i - 1]:
             count_increase += 1
     print("Part 1", count_increase)
 
 
-def part_2(lines):
+def part_2(values):
     count_increase = 0
-    for i in range(3, len(lines)):
-        sum_a = int(lines[i]) + int(lines[i - 1]) + int(lines[i - 2])
-        sum_b = int(lines[i - 1]) + int(lines[i - 2]) + int(lines[i - 3])
+    for i in range(3, len(values)):
+        sum_a = values[i] + values[i - 1] + values[i - 2]
+        sum_b = values[i - 1] + values[i - 2] + values[i - 3]
         if sum_a > sum_b:
             count_increase += 1
     print("Part 2", count_increase)
@@ -21,10 +21,10 @@ def part_2(lines):
 
 def main():
     with open("input_01.txt", 'r') as in01:
-        lines = [line.strip() for line in in01.readlines()]
+        values = [int(line.strip()) for line in in01.readlines()]
     s = time.time()
-    part_1(lines)
-    part_2(lines)
+    part_1(values)
+    part_2(values)
     print(f"Took {time.time() -s:.3f}s")
 
 
