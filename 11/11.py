7c8e56e71c876865fec06aa93a2907431bb86858
Cyril <github@kosmon.fr>
2021-12-11 09:06:35 +0100

    Clean

--- 11/11.py
+++ 11/11.py
@@ -4,15 +4,11 @@ import time
 def neighbors(i):
     y = int(i / 10)
     x = i - 10*y
-
     n = []
-    for j in [x-1, x, x+1]:
-        for k in [y-1, y, y+1]:
-            if y == k and x == j:
-                continue
-            if 0 <= j < 10 and 0 <= k < 10:
-                n.append(10*k+j)
-    return n
+    for j in [j for j in [x-1, x, x+1] if 0 <= j < 10]:
+        for k in [k for k in [y-1, y, y+1] if 0 <= k < 10]:
+            n.append(10*k+j)
+    return [j for j in n if j != i]
 
 
 def evolve(octopuses):
