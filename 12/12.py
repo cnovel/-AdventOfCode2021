6065473bd57ede912a886e9296bbdcde3fd2da00
Cyril <github@kosmon.fr>
2021-12-12 10:00:59 +0100

    Do not need list, only count

--- 12/12.py
+++ 12/12.py
@@ -1,45 +1,44 @@
 import time
-from collections import Counter
 
 
 def find_paths_small_cave_once(edges, cur_path):
-    paths = []
+    count = 0
     last_node = cur_path[-1]
     possible_next_nodes = [edge[1] for edge in edges if edge[0] == last_node] + \
                           [edge[0] for edge in edges if edge[1] == last_node]
     for node in possible_next_nodes:
         if node == 'end':
-            paths.append(cur_path + ['end'])
+            count += 1
             continue
         if node.islower() and node not in cur_path:
-            paths += find_paths_small_cave_once(edges, cur_path + [node])
+            count += find_paths_small_cave_once(edges, cur_path + [node])
             continue
         if node.isupper():
-            paths += find_paths_small_cave_once(edges, cur_path + [node])
-    return paths
+            count += find_paths_small_cave_once(edges, cur_path + [node])
+    return count
 
 
 def find_paths_small_cave_twice(edges, cur_path, has_small_cave_duplicate):
-    paths = []
+    count = 0
     last_node = cur_path[-1]
     possible_next_nodes = [edge[1] for edge in edges if edge[0] == last_node] + \
                           [edge[0] for edge in edges if edge[1] == last_node]
     for node in possible_next_nodes:
         if node == 'end':
-            paths.append(cur_path + ['end'])
+            count += 1
             continue
         if node.islower():
             if node == 'start':
                 continue
             if node not in cur_path:
-                paths += find_paths_small_cave_twice(edges, cur_path + [node], has_small_cave_duplicate)
+                count += find_paths_small_cave_twice(edges, cur_path + [node], has_small_cave_duplicate)
                 continue
             if not has_small_cave_duplicate:
-                paths += find_paths_small_cave_twice(edges, cur_path + [node], True)
+                count += find_paths_small_cave_twice(edges, cur_path + [node], True)
                 continue
         if node.isupper():
-            paths += find_paths_small_cave_twice(edges, cur_path + [node], has_small_cave_duplicate)
-    return paths
+            count += find_paths_small_cave_twice(edges, cur_path + [node], has_small_cave_duplicate)
+    return count
 
 
 def main():
@@ -48,10 +47,10 @@ def main():
     s = time.time()
 
     edges = [[line.split('-')[0], line.split('-')[1]] for line in lines]
-    paths = find_paths_small_cave_once(edges, ['start'])
-    print("Part 1:", len(paths))
-    paths = find_paths_small_cave_twice(edges, ['start'], False)
-    print("Part 2:", len(paths))
+    nb_paths = find_paths_small_cave_once(edges, ['start'])
+    print("Part 1:", nb_paths)
+    nb_paths = find_paths_small_cave_twice(edges, ['start'], False)
+    print("Part 2:", nb_paths)
     print(f"Took {time.time() - s:.3f}s")
 
 
