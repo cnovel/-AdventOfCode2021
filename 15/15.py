d8116274264f4861669c65ad738d62cce2c1fcdb
Cyril <github@kosmon.fr>
2021-12-15 19:31:36 +0100

    Big revamp for optim

--- 15/15.py
+++ 15/15.py
@@ -11,68 +11,77 @@ def increment_line(line, n=1):
     return new_line
 
 
+def neighbors(i, width, height):
+    y = int(i / width)
+    x = i - width*y
+    n = []
+    if x-1 >= 0:
+        n.append(x-1 + y * width)
+    if x+1 < width:
+        n.append(x+1 + y * width)
+    if y-1 >= 0:
+        n.append(x + (y-1) * width)
+    if y+1 < height:
+        n.append(x + (y+1) * width)
+    return n
+
+
+def dijkstra(lines):
+    cost = [x for line in lines for x in line]
+    width = len(lines[0])
+    height = len(lines)
+
+    dist = [-1] * (width * height)
+    visited = [False] * (width * height)
+    dist[0] = 0
+
+    to_visit = width * height
+    modified_pt = {0}
+    while to_visit > 0:
+        pt = (-1, -1)
+        best_dist = -1
+        # Find next point to consider
+        for p in modified_pt:
+            if best_dist == -1 or dist[p] < best_dist:
+                best_dist = dist[p]
+                pt = p
+
+        # Mark it as visited
+        visited[pt] = True
+        modified_pt.remove(pt)
+        to_visit -= 1
+
+        # Retrieve unvisited neighbors and update distance
+        neighs = set([n for n in neighbors(pt, width, height) if not visited[n]])
+        modified_pt = modified_pt.union(neighs)
+        for n in neighs:
+            d = dist[pt] + cost[n]
+            if d < dist[n] or dist[n] == -1:
+                dist[n] = d
+
+    return dist[width*height - 1]
+
+
 def main():
     with open("input_15.txt", 'r') as in15:
         lines = [line.strip() for line in in15.readlines()]
     s = time.time()
 
     lines = [[int(x) for x in line] for line in lines]
+    print("Part 1:", dijkstra(lines))
+    print(f"Took {time.time() - s:.3f}s")
 
-    """
     height = len(lines)
     for n in range(1, 5):
         for line in lines[0:height]:
             lines.append(increment_line(line, n))
-
     new_lines = []
     for line in lines:
         line = line + increment_line(line, 1) + increment_line(line, 2) \
                + increment_line(line, 3) + increment_line(line, 4)
         new_lines.append(line)
+    print("Part 2:", dijkstra(new_lines))
 
-    lines = new_lines
-    """
-
-    dist = {}
-    q = []
-    width = len(lines[0])
-    height = len(lines)
-    for i in range(0, width):
-        for j in range(0, height):
-            dist[(i, j)] = -1
-            q.append((i, j))
-    dist[(0, 0)] = 0
-
-    find_total = 0
-    pop_time = 0
-    neigh_time = 0
-    while len(q) > 0:
-        pt = (-1, -1)
-        best_dist = -1
-        start = time.time()
-        for p in q:
-            if dist[p] != -1:
-                if best_dist == -1 or dist[p] < best_dist:
-                    best_dist = dist[p]
-                    pt = p
-        find_total += time.time() - start
-        start = time.time()
-        q.remove(pt)
-        pop_time += time.time() - start
-        if pt == (width-1, height-1):
-            print(f"Dist part 1 = {dist[(width-1, height-1)]}")
-        start = time.time()
-        neighs = [(pt[0] - 1, pt[1]), (pt[0] + 1, pt[1]), (pt[0], pt[1] - 1), (pt[0], pt[1] + 1)]
-        neighs = [n for n in neighs if n in q]
-        neigh_time += time.time() - start
-        for n in neighs:
-            d = dist[pt] + lines[n[1]][n[0]]
-            if d < dist[n] or dist[n] == -1:
-                dist[n] = d
-    print(dist[(width-1, height-1)])
-    print("Find total:", find_total)
-    print("Pop:", pop_time)
-    print("Neigh:", neigh_time)
     print(f"Took {time.time() - s:.3f}s")
 
 
