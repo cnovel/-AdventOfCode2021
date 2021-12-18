514a4b02930fc74698a57db90e10da225e821277
Cyril <github@kosmon.fr>
2021-12-18 11:42:13 +0100

    18 in progress, can't figure out why processing fails

---
+++ 18/18.py
@@ -0,0 +1,111 @@
+import time
+
+
+def parse_line(line):
+    snail = []
+    level = 0
+    for x in line:
+        if x == '[':
+            level += 1
+        elif x == ']':
+            level -= 1
+        elif x == ',':
+            continue
+        else:
+            snail.append([int(x), level])
+    return snail
+
+
+def add_snail(snail_a, snail_b):
+    new_snail = []
+    for x in snail_a:
+        new_snail.append([x[0], x[1]+1])
+    for x in snail_b:
+        new_snail.append([x[0], x[1]+1])
+    return new_snail
+
+
+def magnitude(snail):
+    # We'll try to reduce from level 4 to level 0
+    step = 4
+    while len(snail) > 1 and step != 0:
+        new_snail = []
+        skip = False
+        for i in range(0, len(snail)):
+            if skip:
+                skip = False
+                continue
+
+            if snail[i][1] == step:
+                mag = 3*snail[i][0] + 2*snail[i+1][0]
+                skip = True
+                new_snail.append([mag, step-1])
+                continue
+
+            new_snail.append(snail[i])
+        snail = new_snail
+        step -= 1
+    return snail[0][0]
+
+
+def process_snail(snail):
+    change = True
+    while change:
+        new_snail = []
+        change = False
+        skip = False
+        for i in range(0, len(snail)):
+            if skip:
+                skip = False
+                continue
+
+            if change:
+                new_snail.append(snail[i])
+                continue
+
+            if snail[i][0] > 9:  # Split
+                i_left = int(snail[i][0]/2)
+                i_right = int(snail[i][0]/2 + 0.5)
+                new_snail.append([i_left, snail[i][1] + 1])
+                new_snail.append([i_right, snail[i][1] + 1])
+                change = True
+                continue
+
+            if snail[i][1] > 4:  # Explode
+                if len(new_snail) > 0:
+                    new_snail[-1][0] += snail[i][0]
+                if i+2 < len(snail):
+                    snail[i+2][0] += snail[i+1][0]
+                new_snail.append([0, snail[i][1]-1])
+                skip = True
+                change = True
+                continue
+
+            new_snail.append(snail[i])
+
+        snail = new_snail
+        print(snail)
+    return snail
+
+
+def main():
+    with open("input_18test.txt", 'r') as in18:
+        lines = [line.strip() for line in in18.readlines()]
+    s = time.time()
+
+    snails = [parse_line(line) for line in lines]
+
+    snail = process_snail(snails[0])
+    print(snail)
+    for i in range(1, len(snails)):
+        snail = add_snail(snail, snails[i])
+        print("Add:\n", snail)
+        snail = process_snail(snail)
+
+    print("Part 1:", magnitude(snail))
+
+    print(f"Took {time.time() - s:.3f}s")
+
+
+if __name__ == '__main__':
+    main()
